ef5d6763a4efa0bf72ae98bc0d01f524159e069b
Miras Auyezkhanov <miras.auyezkhanov@gmail.com>
2024-10-16 01:04:21 +0500

    Task is done

---
+++ src/main/java/org/mirasruntime/requestparamtask1miras/model/Movie.java
@@ -0,0 +1,15 @@
+package org.mirasruntime.requestparamtask1miras.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Movie {
+    private String title;
+    private String mpa;
+    private String genre;
+    private double rating;
+}
